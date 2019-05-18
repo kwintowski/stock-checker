7265282d1ed84e8fb28e913e379c7b1b7a57a1f4
Glitch (cake-hawthorn) <none>
2019-05-18 14:34:04 +0000

    Chaining promises

--- routes/api.js
+++ routes/api.js
@@ -9,15 +9,21 @@
 'use strict';
 
 var expect = require('chai').expect;
-var MongoClient = require('mongodb');
-
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+var StockHandler = require('../controller/stockHandler.js');
 
 module.exports = function (app) {
 
+  
   app.route('/api/stock-prices')
     .get(function (req, res){
+      var stock = req.query.stock;
       
+      var stockHandler = new StockHandler(app);
+      stockHandler.getStock(stock)
+      .then(function(result) {return {"stockData":{"stock":stock,"price":result.toString()}};})
+      .then()
+      .catch((reject)=>{res.json(reject)});
+      //res.json("Hi");
     });
     
 };
