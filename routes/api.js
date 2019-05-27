e19f31dbcabcb103e4ca062ee114c5e00995f691
Glitch (cake-hawthorn) <none>
2019-05-27 14:49:57 +0000

    Finished all functional tests

--- routes/api.js
+++ routes/api.js
@@ -9,7 +9,7 @@
 'use strict';
 
 var expect = require('chai').expect;
-var StockHandler = require('../controller/stockHandler.js');
+var StockHandler = require('../controllers/stockHandler.js');
 var MongoClient = require('mongodb');
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 const STOCKS = "stocks";
@@ -20,23 +20,48 @@ module.exports = function (app) {
   app.route('/api/stock-prices')
     .get(function (req, res){
     
-      var stock = req.query.stock;
+      var stockDatum = [];
+      
+      if(typeof(req.query.stock)=="string")
+        stockDatum.push(req.query.stock);
+      else 
+        stockDatum = req.query.stock;
+    
       var ip = (req.headers['x-forwarded-for']);// || '').split(',').pop() || 
 //           req.connection.remoteAddress || 
 //           req.socket.remoteAddress || 
 //           req.connection.socket.remoteAddress;
 
-      console.log(ip.substr(0,ip.indexOf(',')));
-      
       MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true }, (err, client) => {
         var db = client.db('myMongoDB');
 
         if(err) console.log('Database error: ' + err);
+        
+        var likeTicked = req.query.like ? true : false;
         var stockHandler = new StockHandler(app);
-        stockHandler.addLike(stock,ip.substr(0,ip.indexOf(',')),db)
-        .then((likes)=>stockHandler.getStock(stock,likes))
-        .then((stockData)=>res.json(stockData))
-        .catch((reject)=>{res.json(reject)});
+        var stockResult = []; //stores the single or multiple result
+        stockDatum.forEach(function(stock) {
+          stockHandler.addOrCountLikes(likeTicked,stock,ip.substr(0,ip.indexOf(',')),db)
+          .then((likes)=>stockHandler.getStock(stock,likes))
+          .then(function(stockData){
+              stockResult.push(stockData);
+                
+              if(stock==stockDatum[stockDatum.length-1]) {//only display result once last result is reached
+                if(stockResult.length==1) res.json({"stockData":stockResult[0]});  
+                else {
+                  let likeDiff = stockResult[0].likes - stockResult[1].likes;
+                  delete stockResult[0].likes;
+                  delete stockResult[1].likes;
+                  
+                  stockResult[0].rel_likes = likeDiff;
+                  stockResult[1].rel_likes = -likeDiff;
+                  res.json({"stockData":stockResult});
+                }
+//                db.close();
+              }
+            })
+          .catch((reject)=>{res.json(reject)});
+          });
       });
   });
     
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -20,29 +20,83 @@ suite('Functional Tests', function() {
       test('1 stock', function(done) {
        chai.request(server)
         .get('/api/stock-prices')
+        .set('x-forwarded-for', '1.2.3.4')
         .query({stock: 'goog'})
         .end(function(err, res){
-          
-          //complete this one too
-          
+          assert.equal(res.status, 200);
+          assert.isObject(res.body, 'response should be an object');
+          assert.property(res.body.stockData, 'stock', 'stockdata has property "stock"');
+          assert.property(res.body.stockData, 'price', 'stockdata has property "price"');
+          assert.property(res.body.stockData, 'likes', 'stockdata has property "likes"');          
           done();
         });
       });
       
       test('1 stock with like', function(done) {
-        
+       chai.request(server)
+        .get('/api/stock-prices?stock=aapl&like=true')
+        .set('x-forwarded-for', '1.2.3.4')
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.isObject(res.body, 'response should be an object');
+          assert.property(res.body.stockData, 'stock', 'stockdata has property "stock"');
+          assert.property(res.body.stockData, 'price', 'stockdata has property "price"');
+          assert.property(res.body.stockData, 'likes', 'stockdata has property "likes"'); 
+          assert.nestedPropertyVal(res.body,'stockData.likes',2);
+          done();
+        });        
       });
       
       test('1 stock with like again (ensure likes arent double counted)', function(done) {
-        
+       chai.request(server)
+        .get('/api/stock-prices')
+        .set('x-forwarded-for', '1.2.3.4')
+        .query({stock: 'aapl'})
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.isObject(res.body, 'response should be an object');
+          assert.property(res.body.stockData, 'stock', 'stockdata has property "stock"');
+          assert.property(res.body.stockData, 'price', 'stockdata has property "price"');
+          assert.property(res.body.stockData, 'likes', 'stockdata has property "likes"'); 
+          assert.nestedPropertyVal(res.body,'stockData.likes',2);
+          done();
+        });        
       });
       
       test('2 stocks', function(done) {
-        
+       chai.request(server)
+        .get('/api/stock-prices?stock=goog&stock=msft')
+        .set('x-forwarded-for', '1.2.3.4')
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.isArray(res.body.stockData, 'StockData object should hold an array');
+          assert.property(res.body.stockData[0], 'stock', 'stockdata has property "stock"');
+          assert.property(res.body.stockData[0], 'price', 'stockdata has property "price"');
+          assert.property(res.body.stockData[0], 'rel_likes', 'stockdata has property "likes"'); 
+          assert.property(res.body.stockData[1], 'stock', 'stockdata has property "stock"');
+          assert.property(res.body.stockData[1], 'price', 'stockdata has property "price"');
+          assert.property(res.body.stockData[1], 'rel_likes', 'stockdata has property "likes"');         
+          done();
+        });        
       });
       
       test('2 stocks with like', function(done) {
-        
+       chai.request(server)
+        .get('/api/stock-prices?stock=msft&stock=intc&like=true')
+        .set('x-forwarded-for', '1.2.3.4')
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.isArray(res.body.stockData, 'StockData object should hold an array');
+          assert.property(res.body.stockData[0], 'stock', 'stockdata has property "stock"');
+          assert.property(res.body.stockData[0], 'price', 'stockdata has property "price"');
+          assert.property(res.body.stockData[0], 'rel_likes', 'stockdata has property "likes"'); 
+          assert.property(res.body.stockData[1], 'stock', 'stockdata has property "stock"');
+          assert.property(res.body.stockData[1], 'price', 'stockdata has property "price"');
+          assert.property(res.body.stockData[1], 'rel_likes', 'stockdata has property "likes"');   
+          assert.nestedPropertyVal(res.body,'stockData[0].rel_likes',2);        
+          assert.nestedPropertyVal(res.body,'stockData[1].rel_likes',-2);
+          done();
+        });         
       });
       
     });
